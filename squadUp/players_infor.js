9cf330746507f351a6fa8a01407afc4aba8a9d38
darklord0710 <112267869+darklord0710@users.noreply.github.com>
2023-01-15 17:21:55 +0700

    Initial commit

---
+++ squadUp/players_infor.js
@@ -0,0 +1,31 @@
+function Player(name, strength, value) {
+    this.ten = name;
+    this.sucmanh = strength;
+    this.giatri = value;
+  }
+  
+  var player1 = new Player("Neymar", 800, 2000);
+  var player2 = new Player("Mbpappe", 1000, 100);
+  var player3 = new Player("Messi", 1000, 2000);
+  var player4 = new Player("Johan Cruijff", 900, 0);
+  var player5 = new Player("Luka Modric", 700, 0);
+  var player6 = new Player("Rodri", 700, 1500);
+  var player7 = new Player("Joan Cancelo", 700, 1500);
+  var player8 = new Player("Saliba", 400, 100);
+  var player9 = new Player("Rudiger", 800, 0);
+  var player10 = new Player("Trent Alexander Arnold", 700, 0);
+  var player11 = new Player("Peter Cech", 400, 300);
+  
+  let players = [
+    player1,
+    player2,
+    player3,
+    player4,
+    player5,
+    player6,
+    player7,
+    player8,
+    player9,
+    player10,
+    player11,
+  ];
